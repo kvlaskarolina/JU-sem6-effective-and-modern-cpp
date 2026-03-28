8ddaf39c8ea5ebe17109ab7aad76e2054521a403
karo <karolinykulas@gmail.com>
2026-03-28 22:33:34 +0100

    ex1

---
+++ lab/lab04/ex_4_3_selectIf.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+using namespace std;
+
+bool biggerThan5(int x) { return x > 5; }
+
+/// TODO
+template... OutContainer<T, Alloc> selectIf(InContainer<T, Alloc> c, Predicate p)
+{
+    // ....
+}
+
+int main()
+{
+    auto print = [](auto v)
+    {
+        for (auto x : v)
+            cout << x << " ";
+        cout << endl;
+    };
+
+    std::vector<int> v = {1, 2, 13, 4, 5, 54};
+    std::list<int> result = selectIf<std::list>(v, biggerThan5);
+    print(result); //  13 54
+
+    auto result2 = selectIf<std::deque>(v, [](int x) -> bool
+                                        { return x % 2; });
+    print(result2); //  1 13 5
+
+    auto result3 = selectIf<std::vector>(result2, biggerThan5);
+    print(result3); //  13
+    return 0;
+}
