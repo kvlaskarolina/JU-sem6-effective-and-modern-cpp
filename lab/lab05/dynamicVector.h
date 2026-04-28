028bbba6fcc9b81d94f2e82e11c1f0d7d88e0375
karo <karolinykulas@gmail.com>
2026-04-28 11:18:45 +0200

    lab05 fix

--- lab/lab05/dynamicVector.h
+++ lab/lab05/dynamicVector.h
@@ -1,145 +1,121 @@
-#include "staticVector.h"
+#ifndef DYNAMIC_VECTOR_H
+#define DYNAMIC_VECTOR_H
 
-class VectorException : public std::runtime_error
-{
-public:
-    explicit VectorException(const std::string &message)
-        : std::runtime_error(message) {}
-};
-
+#include "staticVector.h"  
 template <typename T>
-class Vector<T, 0>
-{
-    T *data_;
-    size_t size_;
+class Vector<T, 0> {
+    size_t  size_;
+    T      *data_;
 
 public:
-    using value_type = T;
-    using size_type = std::size_t;
-    typedef T *pointer;
-    typedef T &reference;
-    typedef const T &const_reference;
+    using value_type      = T;
+    using size_type       = std::size_t;
+    typedef T*       pointer;
+    typedef T&       reference;
+    typedef const T& const_reference;
 
-    Vector(size_type size) : size_(size)
-    {
-        data_ = new T[size_];
+    explicit Vector(size_type size) : size_(size), data_(new T[size]) {
         for (size_type i = 0; i < size_; ++i)
-        {
             data_[i] = T();
-        }
     }
-    Vector(const Vector &v) : size_(v.size_)
-    {
-        data_ = new T[size_];
+
+    Vector(const Vector &v) : size_(v.size_), data_(new T[v.size_]) {
         for (size_type i = 0; i < size_; ++i)
-        {
             data_[i] = v.data_[i];
-        }
     }
-    Vector &operator=(const Vector &m)
+
+    Vector(const std::initializer_list<T> &list)
+        : size_(list.size()), data_(new T[list.size()])
     {
-        if (this != &m)
-        {
-            if (size_ != m.size_)
-            {
-                delete[] data_;
-                size_ = m.size_;
-                data_ = new T[size_];
-            }
-            for (size_type i = 0; i < size_; ++i)
-            {
-                data_[i] = m.data_[i];
-            }
+        size_type i = 0;
+        for (const auto &elem : list)
+            data_[i++] = elem;
+    }
+
+    // static -> dynamic
+    template <size_t N>
+    explicit Vector(const Vector<T, N> &v) : size_(N), data_(new T[N]) {
+        for (size_type i = 0; i < size_; ++i)
+            data_[i] = v[i];
+    }
+
+    Vector(Vector &&other) noexcept
+    : size_(other.size_), data_(other.data_)
+    {
+        other.size_ = 0;
+        other.data_ = nullptr;  
+    }
+
+    Vector &operator=(Vector &&other) noexcept {
+        if (this != &other) {
+            delete[] data_;      
+            size_       = other.size_;
+            data_       = other.data_;
+            other.size_ = 0;
+            other.data_ = nullptr;
         }
         return *this;
     }
-    Vector(const std::initializer_list<T> &list) : size_(list.size())
-    {
-        data_ = new T[size_];
-        size_type i = 0;
-        for (const auto &elem : list)
-        {
-            data_[i++] = elem;
+
+    ~Vector() { delete[] data_; }
+
+    Vector &operator=(const Vector &m) {
+        if (this != &m) {
+            if (size_ != m.size_) {
+                delete[] data_;
+                size_  = m.size_;
+                data_  = new T[size_];
+            }
+            for (size_type i = 0; i < size_; ++i)
+                data_[i] = m.data_[i];
         }
+        return *this;
     }
-    template <size_t N>
-    Vector(const Vector<T, N> &v) : size_(N)
-    {
-        data_ = new T[size_];
-        for (size_type i = 0; i < size_; ++i)
-        {
-            data_[i] = v.data_[i];
-        }
-    }
-    void resize(size_type new_size)
-    {
-        if (new_size != size_)
-        {
+
+    void resize(size_type new_size) {
+        if (new_size != size_) {
             T *new_data = new T[new_size];
             for (size_type i = 0; i < std::min(size_, new_size); ++i)
-            {
                 new_data[i] = data_[i];
-            }
             for (size_type i = size_; i < new_size; ++i)
-            {
                 new_data[i] = T();
-            }
             delete[] data_;
             data_ = new_data;
             size_ = new_size;
         }
     }
-    friend Vector operator+(const Vector &u, const Vector &v)
-    {
+
+    reference       operator[](size_type index)       { return data_[index]; }
+    const_reference operator[](size_type index) const { return data_[index]; }
+    const_reference get(size_type index)        const { return data_[index]; }
+
+    size_type size() const { return size_; }
+
+    friend Vector operator+(const Vector &u, const Vector &v) {
         if (u.size_ != v.size_)
-        {
             throw VectorException("incompatible sizes in Vector::operator+");
-        }
         Vector result(u.size_);
         for (size_type i = 0; i < u.size_; ++i)
-        {
             result.data_[i] = u.data_[i] + v.data_[i];
-        }
         return result;
     }
     template <size_t N>
-    friend Vector operator+(const Vector &u, const Vector<T, N> &v)
-    {
+    friend Vector<T, N> operator+(const Vector &u, const Vector<T, N> &v) {
         if (u.size_ != N)
-        {
             throw VectorException("incompatible sizes in Vector::operator+");
-        }
-        Vector result(u.size_);
-        for (size_type i = 0; i < u.size_; ++i)
-        {
-            result.data_[i] = u.data_[i] + v[i];
-        }
+        Vector<T, N> result;
+        for (size_t i = 0; i < N; ++i)
+            result[i] = u.data_[i] + v[i];
         return result;
     }
-    friend std::ostream &operator<<(std::ostream &out, const Vector &v)
-    {
-        for (size_type i = 0; i < v.size_; ++i)
-        {
+
+    friend std::ostream &operator<<(std::ostream &out, const Vector &v) {
+        for (size_type i = 0; i < v.size_; ++i) {
             out << v.data_[i];
-            if (i + 1 < v.size_)
-                out << " ";
+            if (i + 1 < v.size_) out << " ";
         }
         return out;
     }
-    reference operator[](size_type index)
-    {
-        return data_[index];
-    }
-    const_reference operator[](size_type index) const
-    {
-        return data_[index];
-    }
-    const_reference get(size_type index) const
-    {
-        return data_[index];
-    }
-    int size() const
-    {
-        return size_;
-    }
 };
+
+#endif // DYNAMIC_VECTOR_H
--- lab/lab05/staticVector.h
+++ lab/lab05/staticVector.h
@@ -1,122 +1,101 @@
-
 #ifndef STATIC_VECTOR_H
 #define STATIC_VECTOR_H
 
-#include <memory>
 #include <iostream>
 #include <cassert>
-#include <vector>
-#include <cmath>
+#include <stdexcept>
+#include <string>
+#include <algorithm>
+
+template <typename T, size_t N> class Vector;
+template <typename T>           class Vector<T, 0>;
+
+class VectorException : public std::runtime_error {
+public:
+    explicit VectorException(const std::string &message)
+        : std::runtime_error(message) {}
+};
 
 template <typename T, size_t N>
-class Vector
-{
-    T data[N];
+class Vector {
+    T data_[N];
 
 public:
-    typedef T value_type;
+    typedef T          value_type;
     typedef std::size_t size_type;
-    typedef T *pointer;
-    typedef T &reference;
-    typedef const T &const_reference;
+    typedef T*         pointer;
+    typedef T&         reference;
+    typedef const T&   const_reference;
 
-    Vector()
-    {
-        for (auto &elem : data)
-        {
-            elem = T();
-        }
-    }
-    Vector(const Vector &v)
-    {
+    Vector() {
         for (size_type i = 0; i < N; ++i)
-        {
-            data[i] = v.data[i];
-        }
+            data_[i] = T();
     }
-    Vector &operator=(const Vector &m)
-    {
-        if (this != &m)
-        {
-            for (size_type i = 0; i < N; ++i)
-            {
-                data[i] = m.data[i];
-            }
-        }
-        return *this;
+
+    Vector(const Vector &v) {
+        for (size_type i = 0; i < N; ++i)
+            data_[i] = v.data_[i];
     }
-    Vector(const std::initializer_list<T> &list)
-    {
+
+    Vector(const std::initializer_list<T> &list) {
         assert(list.size() == N);
         size_type i = 0;
         for (const auto &elem : list)
-        {
-            data[i++] = elem;
-        }
+            data_[i++] = elem;
     }
-    Vector(const Vector<T, 0> &v)
-    {
-        assert(v.size() == N);
+
+    // dynamic -> static
+    explicit Vector(const Vector<T, 0> &v) {
+        if (static_cast<size_type>(v.size()) != N)
+            throw VectorException(
+                "Size mismatch: cannot convert dynamic vector of size " +
+                std::to_string(v.size()) +
+                " to static vector of size " +
+                std::to_string(N));
         for (size_type i = 0; i < N; ++i)
-        {
-            data[i] = v.get(i);
-        }
+            data_[i] = v.get(i);
     }
-    template <typename U, size_t M>
-    explicit Vector(const Vector<U, M> &other)
-    {
-        static_assert(M == N, "Cannot convert vectors of different dimensions");
-        for (size_t i = 0; i < N; i++)
-            data[i] = static_cast<T>(other[i]);
+
+    template <typename U>
+    explicit Vector(const Vector<U, N> &other) {
+        for (size_type i = 0; i < N; ++i)
+            data_[i] = static_cast<T>(other[i]);
     }
-    friend Vector operator+(const Vector &u, const Vector &v)
-    {
+
+    Vector &operator=(const Vector &m) {
+        if (this != &m)
+            for (size_type i = 0; i < N; ++i)
+                data_[i] = m.data_[i];
+        return *this;
+    }
+
+    reference       operator[](size_type index)       { return data_[index]; }
+    const_reference operator[](size_type index) const { return data_[index]; }
+    const_reference get(size_type index)        const { return data_[index]; }
+    void            set(size_type index, const_reference value) { data_[index] = value; }
+
+    constexpr size_type size() const { return N; }
+
+    friend Vector operator+(const Vector &u, const Vector &v) {
         Vector result;
         for (size_type i = 0; i < N; ++i)
-        {
-            result.data[i] = u.data[i] + v.data[i];
-        }
+            result.data_[i] = u.data_[i] + v.data_[i];
         return result;
     }
-    const_reference get(size_type index) const
-    {
-        return data[index];
-    }
-    friend Vector operator+(const Vector &u, const Vector<T, 0> &v)
-    {
-        assert(v.size() == N);
+
+    friend Vector operator+(const Vector &u, const Vector<T, 0> &v) {
+        if (static_cast<size_type>(v.size()) != N)
+            throw VectorException("incompatible sizes in Vector::operator+");
         Vector result;
         for (size_type i = 0; i < N; ++i)
-        {
-            result.data[i] = u.data[i] + v.data_[i];
-        }
+            result.data_[i] = u.data_[i] + v.get(i);
         return result;
     }
 
-    constexpr size_type size() const
-    {
-        return N;
-    }
-
-    void set(size_type index, const_reference value)
-    {
-        data[index] = value;
-    }
-
-    reference operator[](size_type index)
-    {
-        return data[index];
-    }
-    const_reference operator[](size_type index) const
-    {
-        return data[index];
-    }
-
-    friend std::ostream &operator<<(std::ostream &out, const Vector &v)
-    {
-        for (auto elem : v.data)
-        {
-            out << elem << " ";
+    friend std::ostream &operator<<(std::ostream &out, const Vector &v) {
+        for (size_type i = 0; i < N; ++i) {
+            out << v.data_[i];
+            if (i + 1 < N) out << " ";
         }
         return out;
     }
