ae4879a28058201e1da3d9b6c6ad371acb920e47
karo <karolinykulas@gmail.com>
2026-05-04 12:31:45 +0200

    ex3,ex4

--- lab/lab08/Filter.h
+++ lab/lab08/Filter.h
@@ -1,6 +1,67 @@
+#pragma once
 
-#ifndef LAB8_FILTER_H
-#define LAB8_FILTER_H
+#include <iterator>
+#include <type_traits>
 
+template<typename C, typename P>
+class Filter {
+public:
+    using Storage = std::conditional_t<
+                        std::is_lvalue_reference<C>::value,
+                        C,
+                        std::remove_reference_t<C>>;
 
-#endif //LAB8_FILTER_H
+    using base_iter  = decltype(std::declval<std::remove_reference_t<C>&>().begin());
+    using value_type = typename std::iterator_traits<base_iter>::value_type;
+
+    Filter(C&& c, P p)
+        : storage_(std::forward<C>(c)), pred_(std::move(p)) {}
+
+    struct iterator {
+        using iterator_category = std::forward_iterator_tag;
+        using value_type        = Filter::value_type;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = const value_type*;
+        using reference         = const value_type&;
+
+        iterator(base_iter cur, base_iter end, const P& pred)
+            : cur_(cur), end_(end), pred_(pred)
+        {
+            skip();
+        }
+
+        reference operator*()  const { return *cur_; }
+        pointer   operator->() const { return &(*cur_); }
+
+        iterator& operator++() { ++cur_; skip(); return *this; }
+        iterator  operator++(int) { iterator tmp = *this; ++(*this); return tmp; }
+
+        bool operator==(const iterator& o) const { return cur_ == o.cur_; }
+        bool operator!=(const iterator& o) const { return cur_ != o.cur_; }
+
+    private:
+        void skip() {
+            while (cur_ != end_ && !pred_(*cur_))
+                ++cur_;
+        }
+
+        base_iter cur_;
+        base_iter end_;
+        const P&  pred_;
+    };
+
+    iterator begin() { return iterator(get().begin(), get().end(), pred_); }
+    iterator end()   { return iterator(get().end(),   get().end(), pred_); }
+
+private:
+    Storage storage_;
+
+    std::remove_reference_t<C>& get() { return storage_; }
+
+    P pred_;
+};
+
+template<typename C, typename P>
+auto make_filter(C&& c, P p) {
+    return Filter<C, P>(std::forward<C>(c), std::move(p));
+}
--- lab/lab08/Matrix.h
+++ lab/lab08/Matrix.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
+#include <iterator>
+#include <numeric> 
 using namespace std;
 
 template <typename T, size_t N, size_t M>
@@ -50,7 +52,6 @@ public:
     constexpr T& operator()(int i, int j) noexcept {
         return data[(i - 1) * M + j - 1];
     }
-
     constexpr const T& operator()(int i, int j) const noexcept {
         return data[(i - 1) * M + j - 1];
     }
@@ -62,7 +63,111 @@ public:
                 r(i, j) = a(i, j) + b(i, j);
         return r;
     }
+
+    struct iterator {
+        using iterator_category = std::forward_iterator_tag;
+        using value_type        = T;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = T*;
+        using reference         = T&;
+
+        iterator(T* p) : ptr(p) {}
+
+        reference  operator*()  const { return *ptr; }
+        pointer    operator->() const { return  ptr; }
+
+        iterator& operator++()    { ++ptr; return *this; }
+        iterator  operator++(int) { iterator tmp = *this; ++ptr; return tmp; }
+
+        bool operator==(const iterator& o) const { return ptr == o.ptr; }
+        bool operator!=(const iterator& o) const { return ptr != o.ptr; }
+
+    private:
+        T* ptr;
+    };
+
+    struct const_iterator {
+        using iterator_category = std::forward_iterator_tag;
+        using value_type        = T;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = const T*;
+        using reference         = const T&;
+
+        const_iterator(const T* p) : ptr(p) {}
+        const_iterator(const iterator& it) : ptr(&(*it)) {}
+
+        reference  operator*()  const { return *ptr; }
+        pointer    operator->() const { return  ptr; }
+
+        const_iterator& operator++()    { ++ptr; return *this; }
+        const_iterator  operator++(int) { const_iterator tmp = *this; ++ptr; return tmp; }
+
+        bool operator==(const const_iterator& o) const { return ptr == o.ptr; }
+        bool operator!=(const const_iterator& o) const { return ptr != o.ptr; }
+
+    private:
+        const T* ptr;
+    };
+
+    struct row_iterator {
+        using iterator_category = std::forward_iterator_tag;
+        using value_type        = T;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = T*;
+        using reference         = T&;
+
+        row_iterator(T* p) : ptr(p) {}
+
+        reference  operator*()  const { return *ptr; }
+        pointer    operator->() const { return  ptr; }
+
+        row_iterator& operator++()    { ++ptr; return *this; }
+        row_iterator  operator++(int) { row_iterator tmp = *this; ++ptr; return tmp; }
+
+        bool operator==(const row_iterator& o) const { return ptr == o.ptr; }
+        bool operator!=(const row_iterator& o) const { return ptr != o.ptr; }
+
+    private:
+        T* ptr;
+    };
+
+    struct col_iterator {
+        using iterator_category = std::forward_iterator_tag;
+        using value_type        = T;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = T*;
+        using reference         = T&;
+
+        col_iterator(T* p, size_t stride) : ptr(p), stride(stride) {}
+
+        reference  operator*()  const { return *ptr; }
+        pointer    operator->() const { return  ptr; }
+
+        col_iterator& operator++()    { ptr += stride; return *this; }
+        col_iterator  operator++(int) { col_iterator tmp = *this; ptr += stride; return tmp; }
+
+        bool operator==(const col_iterator& o) const { return ptr == o.ptr; }
+        bool operator!=(const col_iterator& o) const { return ptr != o.ptr; }
+
+    private:
+        T*     ptr;
+        size_t stride;
+    };
+
+    iterator       begin()       { return iterator(data); }
+    iterator       end()         { return iterator(data + N * M); }
+
+    const_iterator begin() const { return const_iterator(data); }
+    const_iterator end()   const { return const_iterator(data + N * M); }
+
+
+    row_iterator row_begin(int n) { return row_iterator(data + (n - 1) * M); }
+    row_iterator row_end  (int n) { return row_iterator(data + (n - 1) * M + M); }
+
+    col_iterator col_begin(int n) { return col_iterator(data + (n - 1),     M); }
+    col_iterator col_end  (int n) { return col_iterator(data + (n - 1) + N * M, M); }
 };
+
 
 template <typename T, size_t N, size_t M>
 void printMatrix(const Matrix<T, N, M>& m, int width = 10) {
