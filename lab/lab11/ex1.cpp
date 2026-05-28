20db0b8fbde912e0ac582de1a5d36960f344df5c
karo <karolinykulas@gmail.com>
2026-05-28 09:00:35 +0200

    ex11.1

---
+++ lab/lab11/ex1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+long double operator"" _km(long double x){
+    return static_cast<long double>(x * 1000);
+}
+long double operator"" _m(long double x){
+    return static_cast<long double>(x);
+}
+long double operator"" _cm(long double x){
+    return static_cast<long double>(x * 0.01);
+}
+long double operator"" _km(unsigned long long x){
+    return static_cast<long double>(x * 1000);
+}
+long double operator"" _m(unsigned long long x){
+    return static_cast<long double>(x);
+}
+long double operator"" _cm(unsigned long long x){
+    return static_cast<long double>(x * 0.01);
+}
+
+long double operator"" _h(long double x){
+    return static_cast<long double>(x * 3600);
+}
+long double operator"" _s(long double x){
+    return static_cast<long double>(x);
+}
+long double operator"" _ms(long double x){
+    return static_cast<long double>(x * 0.001);
+}
+long double operator"" _h(unsigned long long x){
+    return static_cast<long double>(x * 3600);
+}
+long double operator"" _s(unsigned long long x){
+    return static_cast<long double>(x);
+}
+long double operator"" _ms(unsigned long long x){
+    return static_cast<long double>(x * 0.001);
+}
+
+/**
+    Computes velocity in meters per seconds.
+    @param distance  distance in meters
+    @param time      time in seconds
+    @return velocity in meters per seconds.
+*/
+double computeVelocity(double distance, double time){
+    return distance/time;
+}
+
+int main(){
+    cout << computeVelocity(100_m, 5_s) << endl;     //20
+    cout << computeVelocity(360_km, 2.0_h) << endl;  //50
+    cout << computeVelocity(3.6_km, 0.02_h) << endl; //50
+    cout << computeVelocity(250_cm, 2.5_ms) << endl; //1000
+    return 0;
+}
